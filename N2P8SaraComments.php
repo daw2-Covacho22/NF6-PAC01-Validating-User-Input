541668570ac1b597a9ef49f8c9a8a7e1afb0df5f
daw2-Covacho22 <91278401+daw2-Covacho22@users.noreply.github.com>
2021-11-22 17:54:48 +0100

    Add files via upload

---
+++ N2P8SaraComments.php
@@ -0,0 +1,41 @@
+<html>
+ <head>
+  <title>N2P1SaraComments</title>
+ </head>
+ <body>
+<?php
+	echo'<strong>';
+	echo"Los conceptos más importantes que hayas aprendido en este documento";
+	echo'</strong>';
+	echo '<br>';
+			echo"Las consultas";
+			echo '<br>';
+			echo"Crear los campor";
+			echo '<br>';
+			echo"la organización de las variables";
+			echo '<br>';
+
+	echo'<strong>';
+	echo"Califica el documento (0-10), y la explicación del profesor (0-10)";
+	echo'</strong>';
+	echo '<br>';
+	
+			echo"10 y 10";
+			echo '<br>';
+	echo'<strong>';
+	echo"Como estudiante ponte una nota";
+	echo'</strong>';
+	echo '<br>';
+	
+		echo"10";
+		echo '<br>';
+
+	echo'<strong>';
+	echo"Explica una mejora";
+	echo'</strong>';
+	echo '<br>';
+	
+		echo"El no tener que cambiar el db o mysqli."
+?>
+ </body>
+</html>
